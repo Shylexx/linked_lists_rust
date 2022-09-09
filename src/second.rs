1dce6ea072567f5731c2c6c4540cc02c6de9b5ac
Alex Dauncey <alexanderdauncey@gmail.com>
2022-09-09 17:16:06 +0100

    peek method + test

--- src/second.rs
+++ src/second.rs
@@ -15,7 +15,7 @@ impl<T> List<T> {
     }
     pub fn push(&mut self, elem: T) {
         let new_node = Box::new(Node {
-            elem: elem,
+            elem,
             next: self.head.take(),
         });
 
@@ -25,6 +25,16 @@ impl<T> List<T> {
         self.head.take().map(|node| {
             self.head = node.next;
             node.elem
+        })
+    }
+    pub fn peek(&self) -> Option<&T> {
+        self.head.as_ref().map(|node| {
+            &node.elem
+        })
+    }
+    pub fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| {
+            &mut node.elem
         })
     }
 }
@@ -75,5 +85,16 @@ mod test {
         // Check exhaustion of list elements
         assert_eq!(list.pop(), Some(1));
         assert_eq!(list.pop(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert_eq!(list.peek(), None);
+        assert_eq!(list.peek_mut(), None);
+        list.push(1); list.push(2); list.push(3);
+
+        assert_eq!(list.peek(), Some(&3));
+        assert_eq!(list.peek_mut(), Some(&mut 3));
     }
 }
