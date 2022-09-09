6007808f6cb269534b25c7f3da38e13e63896252
Alex Dauncey <alexanderdauncey@gmail.com>
2022-09-09 22:12:51 +0100

    Peek front to get a ref to the next value via Ref::map

--- src/fourth.rs
+++ src/fourth.rs
@@ -1,4 +1,4 @@
-use std::{rc::Rc, cell::RefCell};
+use std::{rc::Rc, cell::{RefCell, Ref}};
 
 pub struct List<T> {
     head: Link<T>,
@@ -65,6 +65,12 @@ impl<T> List<T> {
             Rc::try_unwrap(old_head).ok().unwrap().into_inner().elem
         })
     }
+    // Uses map to get a Ref to the value held inside the struct
+    pub fn peek_front(&self) -> Option<Ref<T>> {
+        self.head.as_ref().map(|node| {
+            Ref::map(node.borrow(), |node| &node.elem)
+        })
+    }
 }
 
 impl<T> Drop for List<T> {
@@ -105,5 +111,14 @@ mod test {
         // Check exhaustion
         assert_eq!(list.pop_front(), Some(1));
         assert_eq!(list.pop_front(), None);
+    }
+
+    #[test]
+    fn peek() {
+        let mut list = List::new();
+        assert!(list.peek_front().is_none());
+        list.push_front(1); list.push_front(2); list.push_front(3);
+
+        assert_eq!(&*list.peek_front().unwrap(), &3);
     }
 }
