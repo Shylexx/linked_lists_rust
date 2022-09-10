928418a816ab1231446d2b074066444ac217a687
Alex Dauncey <alexanderdauncey@gmail.com>
2022-09-10 03:01:46 +0100

    Production Deque pop and push

--- src/prod_unsafe_deque.rs
+++ src/prod_unsafe_deque.rs
@@ -1,13 +1,124 @@
+use std::{ptr::NonNull, marker::PhantomData};
+
 pub struct LinkedList<T> {
     front: Link<T>,
     back: Link<T>,
     len: usize,
+    _boo: PhantomData<T>,
 }
 
-type Link<T> = *mut Node<T>;
+type Link<T> = Option<NonNull<Node<T>>>;
 
 struct Node<T> {
-    from: Link<T>,
+    front: Link<T>,
     back: Link<T>,
     elem: T,
+}
+
+impl<T> LinkedList<T> {
+    pub fn new() -> Self {
+        Self {
+            front: None,
+            back: None,
+            len: 0,
+            _boo: PhantomData,
+        }
+    }
+    pub fn push_front(&mut self, elem: T) {
+        unsafe {
+            let new = NonNull::new_unchecked(Box::into_raw(Box::new(Node {
+                front: None,
+                back: None,
+                elem,
+            })));
+            if let Some(old) = self.front {
+                // Put the new front in front of the old one
+                (*old.as_ptr()).front = Some(new);
+                (*new.as_ptr()).back = Some(old);
+            } else {
+                // If theres no front, we are the empty list and need to set the back too.
+                debug_assert!(self.back.is_none());
+                debug_assert!(self.front.is_none());
+                debug_assert!(self.len == 0);
+                self.back = Some(new);
+            }
+            self.front = Some(new);
+            self.len += 1;
+        }
+    }
+    pub fn pop_front(&mut self) -> Option<T> {
+        unsafe {
+            // Only do stuff if there is a front to pop
+            // We dont mess with take anymore 
+            // because everything is Copy and no dtors 
+            // will run if we mess up
+            self.front.map(|node| {
+                // Bring box back to life so we can move out its val and 
+                // Drop it (Box will do it for us)
+                let boxed_node = Box::from_raw(node.as_ptr());
+                // The value from the popped node
+                let result = boxed_node.elem;
+                // Make the next node into the new front
+                self.front = boxed_node.back;
+                if let Some(new) = self.front {
+                    // Cleanup reference to removed code
+                    (*new.as_ptr()).front = None;
+                } else {
+                    // If front is now null, the list is now empty
+                    debug_assert!(self.len == 1);
+                    self.back = None;
+                }
+
+                self.len -= 1;
+                result
+                // Box implicitly freed here.
+            })
+        }
+    }
+}
+
+
+#[cfg(test)]
+mod test {
+    use super::LinkedList;
+
+    #[test]
+    fn test_basic_front() {
+        let mut list = LinkedList::new();
+
+        // Try to break an empty list
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+
+        // Try to break a one item list
+        list.push_front(10);
+        assert_eq!(list.len(), 1);
+        assert_eq!(list.pop_front(), Some(10));
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+
+        // Mess around
+        list.push_front(10);
+        assert_eq!(list.len(), 1);
+        list.push_front(20);
+        assert_eq!(list.len(), 2);
+        list.push_front(30);
+        assert_eq!(list.len(), 3);
+        assert_eq!(list.pop_front(), Some(30));
+        assert_eq!(list.len(), 2);
+        list.push_front(40);
+        assert_eq!(list.len(), 3);
+        assert_eq!(list.pop_front(), Some(40));
+        assert_eq!(list.len(), 2);
+        assert_eq!(list.pop_front(), Some(20));
+        assert_eq!(list.len(), 1);
+        assert_eq!(list.pop_front(), Some(10));
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+        assert_eq!(list.pop_front(), None);
+        assert_eq!(list.len(), 0);
+    }
 }
